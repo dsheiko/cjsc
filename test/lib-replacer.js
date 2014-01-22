96e2d1f66d31d08b415e0b1374819f0040e5763c
Dmitry Sheiko <web.sbelarus@gmail.com>
2014-01-22 10:37:18 +0100

    Replacer fixed: it was incorrectly assigning offsets

--- test/lib-replacer.js
+++ test/lib-replacer.js
@@ -19,13 +19,46 @@ describe( "Replacer", function(){
 		it( "must", function(){
 			replacer.replace( 3,4, "---------" );
 			replacer.replace( 5,5, "***" );
-			replacer.get().should.eql( "123---------567***89" );
+			replacer.get().should.eql( "123---------5***6789" );
 		});
 		it( "must", function(){
 			replacer.replace( 3,4, "---------" );
 			replacer.replace( 5,5, "***" );
 			replacer.replace( 7,7, "+" );
-			replacer.get().should.eql( "123---------567***89+" );
+			replacer.get().should.eql( "123---------5***67+89" );
 		});
 	});
+
+	describe( " with real code ", function(){
+		var replacer;
+		beforeEach(function(){
+			replacer = new Replacer( "console.log( \"main.js running...\" );\n\
+ console.log(require( \"./lib/dep1\" ));\n\
+ console.log(require( \"./lib/dep1\" ));\n" );
+		});
+
+		it( "must", function(){
+			replacer.replace(  50, 73, "require( \"/repositories/home/sheiko/vhosts/os.htdocs/cjsc/demo/lib/dep1.js\" )" );
+			replacer.get().should.eql( "console.log( \"main.js running...\" );\n\
+ console.log(require( \"/repositories/home/sheiko/vhosts/os.htdocs/cjsc/demo/lib/dep1.js\" ));\n\
+ console.log(require( \"./lib/dep1\" ));\n" );
+		});
+		it( "must", function(){
+			replacer.replace(  89, 112, "require( \"/repositories/home/sheiko/vhosts/os.htdocs/cjsc/demo/lib/dep1.js\" )" );
+			replacer.get().should.eql( "console.log( \"main.js running...\" );\n\
+ console.log(require( \"./lib/dep1\" ));\n\
+ console.log(require( \"/repositories/home/sheiko/vhosts/os.htdocs/cjsc/demo/lib/dep1.js\" ));\n" );
+		});
+
+
+		it( "must", function(){
+			replacer.replace(  50, 73, "require( \"/repositories/home/sheiko/vhosts/os.htdocs/cjsc/demo/lib/dep1.js\" )" );
+			replacer.replace(  89, 112, "require( \"/repositories/home/sheiko/vhosts/os.htdocs/cjsc/demo/lib/dep1.js\" )" );
+						replacer.get().should.eql( "console.log( \"main.js running...\" );\n\
+ console.log(require( \"/repositories/home/sheiko/vhosts/os.htdocs/cjsc/demo/lib/dep1.js\" ));\n\
+ console.log(require( \"/repositories/home/sheiko/vhosts/os.htdocs/cjsc/demo/lib/dep1.js\" ));\n" );
+		});
+	});
+
+
 });
